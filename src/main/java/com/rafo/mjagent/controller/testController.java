6b3509b49acdaac973fedf9ffcca0d4136e2bd01
SingleLa <394950814@qq.com>
2017-09-08 14:58:09 +0800

    Initial commit

---
+++ src/main/java/com/rafo/mjagent/controller/testController.java
@@ -0,0 +1,25 @@
+package com.rafo.mjagent.controller;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/9/8.
+ */
+@Controller
+public class testController {
+
+    @RequestMapping("/test.do")
+    @ResponseBody
+    public Map test(Model model){
+        Map m = new HashedMap();
+        m.put("test","asdasd");
+      return m;
+    }
+}
