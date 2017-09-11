3c5753047fa5cba9e6d0aec5d8ee10ba68ff4371
SingleLa <394950814@qq.com>
2017-09-11 12:44:43 +0800

    编码格式

--- src/main/java/com/rafo/mjagent/controller/testController.java
+++ src/main/java/com/rafo/mjagent/controller/testController.java
@@ -4,8 +4,6 @@ import org.apache.commons.collections.map.HashedMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -18,7 +16,7 @@ public class testController {
     @RequestMapping("/test.do")
     public String test(Model model){
         Map m = new HashedMap();
-        m.put("test","asdasd");
+        m.put("test","你好");
         model.addAttribute("m",m);
       return "index";
     }
